e9141ddd7421bd23ce51406c40d0d487201ad53f
subham sharma <subham.sharma@smartbear.com>
2024-09-03 15:25:40 +0530

    login no auth

--- src/main/java/com/sam/crickstatloginservice/controller/UserController.java
+++ src/main/java/com/sam/crickstatloginservice/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.sam.crickstatloginservice.controller;
 
 import com.sam.crickstatloginservice.dto.UserDTO;
+import com.sam.crickstatloginservice.entity.UserEntity;
+import com.sam.crickstatloginservice.repository.UserRepository;
 import com.sam.crickstatloginservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,20 +13,73 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
+
+//    @Autowired
+//    private UserEntity userEntity;
+//    @PostMapping("/register")
+//    public ResponseEntity<UserDTO> registerUser(@RequestBody UserDTO userDTO ) {
+//        try {
+//            UserDTO registeredUser = userService.signup(userDTO);
+//            return new ResponseEntity<>(registeredUser, HttpStatus.OK);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+//        }
+//    }
+
+
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> registerUser(@RequestBody UserDTO userDTO ) {
+    public ResponseEntity<ResponseMessage> registerUser(@RequestBody UserDTO userDTO) {
         try {
-            UserDTO registeredUser = userService.signup(userDTO);
-            return new ResponseEntity<>(registeredUser, HttpStatus.OK);
+            if (userRepository.existsByEmail(userDTO.getEmail())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(new ResponseMessage("Email already in use"));
+            }
+            if (userRepository.existsByUsername(userDTO.getUsername())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(new ResponseMessage("Username already in use"));
+            }
+
+            UserEntity userEntity = userService.convertToUserEntity(userDTO);
+            userEntity = userRepository.save(userEntity);
+
+            return ResponseEntity.ok(new ResponseMessage("User registered successfully"));
+
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseMessage("An unexpected error occurred"));
         }
     }
+
+    // Define a simple class to encapsulate the response message
+    static class ResponseMessage {
+        private String message;
+
+        public ResponseMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+
+
+
+
+
+
 
     @PostMapping("/login")
     public ResponseEntity<UserDTO> loginUser(@RequestBody Map<String, String> loginRequest) {
